6b0ed8158abde1b5bbc05c614fc6b033a1b0616a
Sukrtia2134 <sugritabursuk@gmail.com>
2025-09-24 18:29:28 +0700

    finished

--- src/routes/courseRoutes.ts
+++ src/routes/courseRoutes.ts
@@ -107,7 +107,6 @@ router.post("/courses", (req: Request, res: Response) => {
     const result = zCoursePostBody.safeParse(body); // check zod
     if (!result.success) {
       return res.status(400).json({
-        success: false,
         message: "Validation failed",
         errors: result.error.issues[0]?.message,
       });
@@ -154,6 +153,7 @@ router.put("/courses", (req: Request, res: Response) => {
     const result = zCoursePutBody.safeParse(body); // check zod
     if (!result.success) {
       return res.status(400).json({
+        success: false,
         message: "Validation failed",
         errors: result.error.issues[0]?.message,
       });
